752c0c04e6d8c3bc8a9f58229c6a48dc4941d92b
Matt Gehrls <mattgehrls@gmail.com>
2023-04-26 17:47:21 -0400

    Setting up a placeholder

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+
+    return (
+        <div className="flex justify-center">
+            <div className="flex flex-col items-center py-6">
+            <h1 className="text-3xl">Matt Gehrls</h1>
+            <h3>Software Development</h3>
+            </div>
+        </div>
+    );
+    };
+
+    export default Header;
