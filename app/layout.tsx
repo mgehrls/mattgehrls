79d6e829ce5594aa1eba6b1741a5afaf8c18cffe
Matt Gehrls <mattgehrls@gmail.com>
2024-10-08 15:52:01 -0400

    light/dark upgrade

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
           className={clsx(
             "mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0 relative",
             inter.className,
-            "bg-gray-200 dark:bg-slate-900",
-            "text-gray-700 dark:text-slate-400"
+            "bg-slate-200 dark:bg-slate-900",
+            "text-slate-700 dark:text-slate-400"
           )}
         >
           {children}
--- components/main/Work.tsx
+++ components/main/Work.tsx
@@ -1,13 +1,10 @@
 import { WorkItemProps, workItems } from "@/utils/workData";
+import { ExternalLink } from "lucide-react";
 import React from "react";
 
 function Work() {
   return (
-    <section
-      id="work"
-      className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
-      aria-label="Work"
-    >
+    <section id="work" className="flex flex-col gap-8 mb-20" aria-label="Work">
       {workItems.map((workItem) => (
         <WorkItem key={workItem.title} {...workItem} />
       ))}
@@ -27,46 +24,39 @@ function WorkItem({
 }: WorkItemProps) {
   return (
     <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50">
-      <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+      <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:dark:bg-slate-800/50 lg:group-hover:bg-slate-400/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
       <div className="z-10 sm:col-span-6">
-        <h3 className="font-medium leading-snug text-slate-200">
+        <h3 className="font-medium leading-snug text-slate-900 dark:text-slate-200">
           <div>
             <a
-              className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+              className="inline-flex items-center font-medium leading-tight text-slate-900 dark:text-slate-200 group-hover:text-indigo-900 focus-visible:text-indigo-900 group-hover:dark:text-teal-300 focus-visible:dark:text-teal-300 text-base"
               href={link}
               target="_blank"
               rel="noreferrer noopener"
               aria-label={linkAriaLabel}
             >
-              <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
-              <span>
+              <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block" />
+              <span className="font-bold dark:font-normal">
                 {title}
                 {" - "}
-                <span className="inline-block">
-                  {where}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                </span>
+                {where}
               </span>
+              {link && (
+                <ExternalLink
+                  size={16}
+                  className="inline-block ml-1.5 group-hover:scale-110"
+                />
+              )}
             </a>
           </div>
         </h3>
-        <p className="mt-2 text-sm leading-normal">{description}</p>
+        <p className="mt-2 text-sm leading-normal text-slate-700 dark:text-slate-400">
+          {description}
+        </p>
         <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
           {techUsed.map((tech) => (
             <li className="mr-1.5 mt-2" key={tech}>
-              <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300 ">
+              <div className="flex items-center rounded-full bg-indigo-900 text-indigo-200 dark:bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 dark:text-teal-300 ">
                 {tech}
               </div>
             </li>
