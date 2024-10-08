16591a0777fd6501073bd064045a5cab0fce7e52
Matt Gehrls <mattgehrls@gmail.com>
2024-10-08 14:59:13 -0400

    removed unused classes

--- components/ThemeChanger.tsx
+++ components/ThemeChanger.tsx
@@ -7,7 +7,7 @@ export const ThemeChanger = () => {
     <div className="absolute top-4 right-4">
       {theme === "light" ? (
         <button
-          className="text-lightModeText border-2 border-lightModeText p-[6px] rounded-lg hover:scale-110 transition-all duration-150 ease-in-out"
+          className="p-[6px] rounded-lg hover:scale-110 transition-all duration-150 ease-in-out"
           onClick={() => setTheme("dark")}
         >
           <Moon />
@@ -15,7 +15,7 @@ export const ThemeChanger = () => {
         </button>
       ) : (
         <button
-          className="border-2 p-[6px] border-darkModeBodyText rounded-lg hover:scale-110 transition-all duration-150 ease-in-out"
+          className="p-[6px] rounded-lg hover:scale-110 transition-all duration-150 ease-in-out"
           onClick={() => setTheme("light")}
         >
           <Sun />
